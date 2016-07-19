8a9e5b321e8487ed5d782c4f317d1e9baa1cf3ae
stephen-standridge <stephen@formisfunction.io>
2016-07-18 22:36:35 -0700

    start creating the first version

---
+++ src/configuration.js
@@ -0,0 +1,23 @@
+module.exports =  function(){
+  return {
+    pixel: 15,
+    offset: { x: -1, y: -1 },
+    mutation: 25,
+    counter: {x: 0, y: 0},
+    color: [255, 255, 255, 1],
+    structure: {
+      config: {
+        depth: 5,
+        branches: 2
+      },
+      data: [{
+        value: {
+          canDivide: true,
+          coords: {x: 170, y:15},
+          dimensions: {width: 300, height: 600},
+          color: 'rgba(255,255,255,1)',
+        }
+      }]
+    }
+  }
+}
